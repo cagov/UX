67ee1cec03e8d2ccdd49b05a24f2c9b41607878b
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-01-31 10:34:46 -0800

    commit caltrans updates

--- docs/caltrans/index.js
+++ docs/caltrans/index.js
@@ -33,4 +33,52 @@ export default function addListeners() {
   })
 }
 
-addListeners();
+let startCoords, endCoords;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25oYW5zIiwiYSI6ImNqNGs4cms1ZzBocXkyd3FzZGs3a3VtamYifQ.HQjFfVzwwxwCmGr2nvnvSA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-79.4512, 43.6568],
+  zoom: 13
+});
+  
+var geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl
+}).on('result',function(item) {
+  document.querySelector('.js-destination-address').innerHTML = `<p class="lead bold mb-0">Destination address:</p>
+  ${item.result.place_name}`;
+  document.querySelector('.js-destination-address').style.display = 'block';
+  endCoords = item.result.center;
+
+  document.querySelector('.js-start-input').style.display = 'block'
+})
+   
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+var geocoderStart = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl
+}).on('result',function(item) {
+  document.querySelector('.js-starting-address').innerHTML = `<p class="lead bold mb-0">Starting address:</p>
+  ${item.result.place_name}`;
+  document.querySelector('.js-starting-address').style.display = 'block';
+  startCoords = item.result.center;
+  displayObs();
+})
+
+document.getElementById('geocoderStart').appendChild(geocoderStart.onAdd(map));
+
+function displayObs() {
+  getDirections(`coordinates=${startCoords};${endCoords}&steps=true&banner_instructions=true`)
+  .then((data) => {
+    let stepMap = getSteps(data);
+
+    getObstructions(stepMap, { startCoords, endCoords }, function(myObstructions) {
+      document.querySelector('.obstructions-major').innerHTML = createHTML(myObstructions);
+      // document.querySelector('.obstructions-minor').innerHTML = minorhtml;
+      location.hash = "#obstructions";
+    });
+  });
+}
