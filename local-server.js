e5ac4b0b3ce7f6abda82d9f9de1cd4c0734dc63a
carterm <carter.medlin@gmail.com>
2020-02-11 13:11:38 -0800

    Fixing relative UX links

--- local-server.js
+++ local-server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 1338
 
-app.use('/', express.static('docs/', {}));
+app.use('/UX', express.static('docs/', {}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!\nhttp://localhost:1338`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!\nhttp://localhost:1338/UX/`))
