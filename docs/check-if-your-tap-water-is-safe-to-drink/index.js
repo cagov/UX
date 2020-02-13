95fa2f9e96fc303e96c0c69f94a442ceaadab333
Rebecca Huval <55514272+humancenteredhuman@users.noreply.github.com>
2020-02-13 13:54:06 -0800

    Update index.js

--- docs/check-if-your-tap-water-is-safe-to-drink/index.js
+++ docs/check-if-your-tap-water-is-safe-to-drink/index.js
@@ -46,7 +46,7 @@ window.geocoder = new MapboxGeocoder({
         }
         website_blurb_1 = ` Your water system has the most detailed information about your water quality. <a href="${website}" target="_self">Visit your water system</a>`;
       }
-      let systemInfo = `<h3>What we track in your water </h3>
+      let systemInfo = `<h3>How we check your water safety</h3>
         <i class="ca-gov-icon-medical-heart text-danger lead float-left pr-2"></i>
         <h4>Health</h4>
         <p>Our scientists watch out for chemicals and bacteria that could be harmful to human health. Public water
